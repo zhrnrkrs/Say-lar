3ed6963a440c9f22fa8ccce36af75becf15f508e
zhrnrkrs <zehranurrkaras@gmail.com>
2026-04-01 12:54:19 +0300

    Switch Case

---
+++ switch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  
+    // Switch variable
+    int var = 1;
+    
+    // Switch statement
+    switch (var) {
+    case 1:
+        printf("Case 1 is Matched.");
+        break;
+    case 2:
+        printf("Case 2 is Matched.");
+        break;
+    case 3:
+        printf("Case 3 is Matched.");
+        break;
+    default:
+        printf("Default case is Matched.");
+        break;
+    }
+    return 0;
+}
